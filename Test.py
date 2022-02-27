ffdeee4d0dcc94fdab1d852a74dd4e942a14f701
Polar Bear <69587185+PolarBearGithub-sudo@users.noreply.github.com>
2022-02-27 20:47:11 +0200

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,15 @@
+from json.encoder import INFINITY
+import time
+vaeee = 0
+dade = 1
+while dade == 1:
+    print("Loading: ", vaeee ," %" ) 
+    vaeee += 1
+    if vaeee > INFINITY:
+        print("Loading:", vaeee,"%" ) 
+        print("W-wait. Infinty?")
+        time.sleep(2)
+        print("Is it greater than infinity?")
+        time.sleep(2)
+        print("Yes.")
+        break
