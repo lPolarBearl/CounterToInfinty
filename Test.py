69f4e56989cb0cd3bddfa3454ad5594177d13ded
Polar Bear <69587185+PolarBearGithub-sudo@users.noreply.github.com>
2023-02-24 21:58:35 +0100

    Abbreviation

--- Test.py
+++ Test.py
@@ -1,15 +1,9 @@
 from json.encoder import INFINITY
-import time
 vaeee = 0
 dade = 1
 while dade == 1:
-    print("Loading: ", vaeee ," %" ) 
+    print(f"Loading: {vaeee}%") 
     vaeee += 1
     if vaeee > INFINITY:
-        print("Loading:", vaeee,"%" ) 
-        print("W-wait. Infinty?")
-        time.sleep(2)
-        print("Is it greater than infinity?")
-        time.sleep(2)
-        print("Yes.")
+        print("Loading ERROR:", vaeee,"%" ) 
         break
