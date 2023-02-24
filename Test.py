21a4ecfd0ce1b7d469435718c9464195dde81c0b
Polar Bear <69587185+PolarBearGithub-sudo@users.noreply.github.com>
2023-02-24 22:02:14 +0100

    Abbreviation
    I abbreviated my code.
    - import
    + f-String

--- Test.py
+++ Test.py
@@ -1,9 +1,8 @@
-from json.encoder import INFINITY
 vaeee = 0
 dade = 1
 while dade == 1:
     print(f"Loading: {vaeee}%") 
     vaeee += 1
-    if vaeee > INFINITY:
-        print("Loading ERROR:", vaeee,"%" ) 
+    if vaeee > float('inf'):
+        print(f"Loading ERROR: {vaeee}%") 
         break
